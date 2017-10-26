4f759c4f6137b4a01dc2c47f92a028330973a71e
Volodymyr Prokopyuk <volodymyr.prokopyuk@ba.com>
2017-10-26 16:01:01 +0200

    feat(ps5.ch02): add MessageRenderer example

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Kotlin](https://kotlinlang.org/) Spring Code Examples
+
+- [Pro Spring 5](http://www.apress.com/us/book/9781484228074) (org.vld.spring.ps5)
---
+++ src/main/kotlin/org/vld/spring/ps5/ch02/Main.kt
@@ -0,0 +1,28 @@
+package org.vld.spring.ps5.ch02
+
+import org.springframework.context.ApplicationContext
+import org.springframework.context.support.ClassPathXmlApplicationContext
+
+interface MessageProvider {
+    val message: String
+}
+
+interface MessageRenderer {
+    val messageProvider: MessageProvider
+    fun render(): String
+}
+
+class LiteralMessageProvider(override val message: String = "Hello Svitlana") : MessageProvider
+
+class PlainTextMessageRenderer(override val messageProvider: MessageProvider) : MessageRenderer {
+    override fun render(): String {
+        println(messageProvider.message)
+        return messageProvider.message
+    }
+}
+
+fun main(args: Array<String>) {
+    val context: ApplicationContext = ClassPathXmlApplicationContext("config/spring/app-context.xml")
+    val messageRenderer: MessageRenderer = context.getBean("plainTextMessageRenderer", MessageRenderer::class.java)
+    messageRenderer.render()
+}
