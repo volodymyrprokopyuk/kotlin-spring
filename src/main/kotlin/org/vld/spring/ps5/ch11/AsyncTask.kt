1b8a8a207e1b49b1a1533db0e6ea465cc4371c59
Volodymyr Prokopyuk <volodymyr.prokopyuk@ba.com>
2017-10-31 16:23:21 +0100

    feat(ps5.ch11): add TaskExecutor example

--- src/main/kotlin/org/vld/spring/ps5/ch11/AsyncTask.kt
+++ src/main/kotlin/org/vld/spring/ps5/ch11/AsyncTask.kt
@@ -4,8 +4,12 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.core.task.TaskExecutor
 import org.springframework.scheduling.annotation.Async
+import org.springframework.scheduling.annotation.AsyncResult
 import org.springframework.stereotype.Component
+import java.util.concurrent.Future
+import javax.annotation.Resource
 
 @Component("asyncTask")
 open class AsyncTask {
@@ -20,13 +24,45 @@ open class AsyncTask {
         logger.info("Simple async task finished")
     }
 
+    @Async
+    fun asyncTaskWithParametersAndReturnValue(task: String): Future<String> {
+        Thread.sleep(1000)
+        return AsyncResult<String>("$task finished")
+    }
+
+}
+
+@Component("asyncTaskExecutor")
+class AsyncTaskExecutor {
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(AsyncTaskExecutor::class.java)
+    }
+
+    @Resource(name = "simpleAsyncTaskExecutor")
+    lateinit var taskExecutor: TaskExecutor
+
+    fun executeAsyncTasks() = (1..2).forEach { task -> taskExecutor.execute { logger.info("Task $task finished") } }
+
 }
 
 fun main(args: Array<String>) {
+    val logger: Logger = LoggerFactory.getLogger("org.vld.spring.ps5.ch11.MainKt")
     val context: ApplicationContext = AnnotationConfigApplicationContext(AsyncTaskConfiguration::class.java)
     val asyncTask: AsyncTask = context.getBean("asyncTask", AsyncTask::class.java)
+    // start simple async tasks
     asyncTask.simpleAsyncTask()
     asyncTask.simpleAsyncTask()
-
-    Thread.sleep(2000)
+    // start async tasks and store Future result
+    val asyncResult1 = asyncTask.asyncTaskWithParametersAndReturnValue("Task 1")
+    val asyncResult2 = asyncTask.asyncTaskWithParametersAndReturnValue("Task 2")
+    // wait for all task completion
+    Thread.sleep(1100)
+    // retrieve Future result
+    logger.info(asyncResult1.get())
+    logger.info(asyncResult2.get())
+    // execute async tasks using TaskExecutor
+    val asyncTaskExecutor: AsyncTaskExecutor = context.getBean("asyncTaskExecutor", AsyncTaskExecutor::class.java)
+    asyncTaskExecutor.executeAsyncTasks()
+    Thread.sleep(500)
 }
--- src/main/kotlin/org/vld/spring/ps5/ch11/AsyncTaskConfiguration.kt
+++ src/main/kotlin/org/vld/spring/ps5/ch11/AsyncTaskConfiguration.kt
@@ -1,10 +1,19 @@
 package org.vld.spring.ps5.ch11
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.SimpleAsyncTaskExecutor
+import org.springframework.core.task.TaskExecutor
 import org.springframework.scheduling.annotation.EnableAsync
 
 @Configuration
 @ComponentScan(basePackages = arrayOf("org.vld.spring.ps5.ch11"))
 @EnableAsync
-open class AsyncTaskConfiguration
+open class AsyncTaskConfiguration {
+
+    // creates new thread on each invocation, does not reuse existing threads
+    @Bean
+    open fun simpleAsyncTaskExecutor(): TaskExecutor = SimpleAsyncTaskExecutor()
+
+}
