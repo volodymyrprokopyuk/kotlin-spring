ea9743513262e3922b6c40ecf0c02262bfd16005
Volodymyr Prokopyuk <volodymyr.prokopyuk@ba.com>
2017-10-31 10:09:27 +0100

    feat(ps5.ch04): all SLF4J Simple implementation

--- src/main/kotlin/org/vld/spring/ps5/ch04/Employee.kt
+++ src/main/kotlin/org/vld/spring/ps5/ch04/Employee.kt
@@ -1,5 +1,7 @@
 package org.vld.spring.ps5.ch04
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.DisposableBean
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.context.support.ClassPathXmlApplicationContext
@@ -8,6 +10,8 @@ import javax.annotation.PreDestroy
 
 class Employee : InitializingBean, DisposableBean {
 
+    val logger: Logger = LoggerFactory.getLogger(Employee::class.java)
+
     var postConstructCalled: Boolean = false
     var afterPropertiesSetCalled: Boolean = false
     var initMethodCalled: Boolean = false
@@ -15,37 +19,37 @@ class Employee : InitializingBean, DisposableBean {
     // add <context:annotation-config/> to XML configuration
     @PostConstruct
     fun postConstruct() {
-        println("** Employee::@postConstruct()")
+        logger.info("** Employee::@postConstruct()")
         postConstructCalled = true
     }
 
     // InitializingBean interface implementation
     override fun afterPropertiesSet() {
-        println("** InitializingBean::afterPropertiesSet()")
+        logger.info("** InitializingBean::afterPropertiesSet()")
         afterPropertiesSetCalled = true
     }
 
     // add <bean init-method="init"/> to XML configuration
     fun initMethod() {
-        println("** Employee::init-method()")
+        logger.info("** Employee::init-method()")
         initMethodCalled = true
     }
 
     // add <context:annotation-config/> to XML configuration
     @PreDestroy
     fun preDestroy() {
-        println("** Employee::@preDestory()")
+        logger.info("** Employee::@preDestory()")
     }
 
     // DisposableBean interface implementation
     override fun destroy() {
-        println("** DisposableBean::destroy()")
+        logger.info("** DisposableBean::destroy()")
     }
 
     // add <bean destroy-method="destroy"/> to XML configuration
     // destroy() is not called by Spring for beans with scope="prototype"
     fun destroyMethod() {
-        println("** Employee::destroy-method()")
+        logger.info("** Employee::destroy-method()")
     }
 
 }
@@ -54,6 +58,6 @@ fun main(args: Array<String>) {
     val context = ClassPathXmlApplicationContext("classpath:config/spring/ch04-employee-app-context.xml")
     context.getBean("employee", Employee::class.java)
     //context.close();
-    // register shutdown hook with JVM
+    // register shutdown hook in new thread with JVM
     context.registerShutdownHook();
 }
