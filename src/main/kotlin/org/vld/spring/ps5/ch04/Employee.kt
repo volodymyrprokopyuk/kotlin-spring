e22b4e90360d14c5f2463d6de0032484d6437cfd
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2017-11-09 19:08:12 +0100

    feat(ps5.ch04): add Java configuration remove XML configuration

--- src/main/kotlin/org/vld/spring/ps5/ch04/Employee.kt
+++ src/main/kotlin/org/vld/spring/ps5/ch04/Employee.kt
@@ -4,7 +4,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.DisposableBean
 import org.springframework.beans.factory.InitializingBean
-import org.springframework.context.support.ClassPathXmlApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import javax.annotation.PostConstruct
 import javax.annotation.PreDestroy
 
@@ -14,33 +14,24 @@ class Employee : InitializingBean, DisposableBean {
         private val logger: Logger = LoggerFactory.getLogger(Employee::class.java)
     }
 
-    var postConstructCalled: Boolean = false
-    var afterPropertiesSetCalled: Boolean = false
-    var initMethodCalled: Boolean = false
-
-    // add <context:annotation-config/> to XML configuration
     @PostConstruct
     fun postConstruct() {
-        logger.info("** Employee::@postConstruct()")
-        postConstructCalled = true
+        logger.info("** Employee::@PostConstruct()")
     }
 
     // InitializingBean interface implementation
     override fun afterPropertiesSet() {
         logger.info("** InitializingBean::afterPropertiesSet()")
-        afterPropertiesSetCalled = true
     }
 
-    // add <bean init-method="init"/> to XML configuration
+    // @Bean(initMethod = "initMethod")
     fun initMethod() {
-        logger.info("** Employee::init-method()")
-        initMethodCalled = true
+        logger.info("** Employee::@Bean(initMethod)")
     }
 
-    // add <context:annotation-config/> to XML configuration
     @PreDestroy
     fun preDestroy() {
-        logger.info("** Employee::@preDestory()")
+        logger.info("** Employee::@PreDestory()")
     }
 
     // DisposableBean interface implementation
@@ -48,16 +39,16 @@ class Employee : InitializingBean, DisposableBean {
         logger.info("** DisposableBean::destroy()")
     }
 
-    // add <bean destroy-method="destroy"/> to XML configuration
+    // @Bean(destroyMethod = "destroyMethod")
     // destroy() is not called by Spring for beans with scope="prototype"
     fun destroyMethod() {
-        logger.info("** Employee::destroy-method()")
+        logger.info("** Employee::@Bean(destroyMethod)")
     }
 
 }
 
 fun main(args: Array<String>) {
-    val context = ClassPathXmlApplicationContext("classpath:config/spring/ch04-employee-app-context.xml")
+    val context = AnnotationConfigApplicationContext(EmployeeConfig::class.java)
     context.getBean("employee", Employee::class.java)
     //context.close()
     // register shutdown hook in new thread with JVM
---
+++ src/main/kotlin/org/vld/spring/ps5/ch04/EmployeeConfig.kt
@@ -0,0 +1,12 @@
+package org.vld.spring.ps5.ch04
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class EmployeeConfig {
+
+    @Bean(initMethod = "initMethod", destroyMethod = "destroyMethod")
+    open fun employee() = Employee()
+
+}
