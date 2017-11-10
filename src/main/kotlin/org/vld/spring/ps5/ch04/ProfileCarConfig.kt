e6c842eba7e8930feb94ab7dc7f02cb44863b7fe
Volodymyr Prokopyuk <volodymyr.prokopyuk@ba.com>
2017-11-10 11:10:35 +0100

    feat(ps5.ch04): add profile application configuration properties

--- src/main/kotlin/org/vld/spring/ps5/ch04/ProfileCarConfig.kt
+++ src/main/kotlin/org/vld/spring/ps5/ch04/ProfileCarConfig.kt
@@ -1,29 +1,45 @@
 package org.vld.spring.ps5.ch04
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.context.annotation.PropertySource
 
 @Configuration
 @Profile("ProfileA")
+@PropertySource("classpath:config/ch04-profileA-application.properties")
 open class ProfileACarConfig {
 
-    @Bean
-    open fun engine(): Engine = Engine("ProfileA V8")
+    @Value("\${ps5.ch04.engine.model}")
+    private lateinit var model: String
+
+    @Value("\${ps5.ch04.car.make}")
+    private lateinit var make: String
 
     @Bean
-    open fun car(): Car = Car("ProfileA BMW", engine())
+    open fun engine(): Engine = Engine(model)
+
+    @Bean
+    open fun car(): Car = Car(make, engine())
 
 }
 
 @Configuration
 @Profile("ProfileB")
+@PropertySource("classpath:config/ch04-profileB-application.properties")
 open class ProfileBCarConfig {
 
-    @Bean
-    open fun engine(): Engine = Engine("ProfileB V8")
+    @Value("\${ps5.ch04.engine.model}")
+    private lateinit var model: String
+
+    @Value("\${ps5.ch04.car.make}")
+    private lateinit var make: String
 
     @Bean
-    open fun car(): Car = Car("ProfileB BMW", engine())
+    open fun engine(): Engine = Engine(model)
+
+    @Bean
+    open fun car(): Car = Car(make, engine())
 
 }
